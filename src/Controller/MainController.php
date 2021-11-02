529227d859aab784d3e84fc70e4f90e2e4d895b8
Kevin BERTAUX <kevinbertaux@gmail.com>
2021-11-02 15:19:59 +0100

    feat: [MAIN CONTROLLER] Affichage des éléments en base sur l'accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(ItemRepository $itemRepository): Response
     {
+        $items = $itemRepository->findBy([], ['addedAt' => 'DESC']);
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'items' => $items
         ]);
     }
 }
